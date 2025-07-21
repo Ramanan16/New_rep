d7d584f0580071590e2746037a4f0c71d645044a
Ramanan16 <chitranesalingam@gmail.com>
2025-07-21 20:37:33 +0530

    add loop in code

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        for(int i = 1 ; i <= 5 ; i++){
+            System.out.println(i + "Hi!");
+        }
     }
 }
