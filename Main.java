146b113af113954d88805f8adadf9a795a175b95
Ramanan16 <138652792+Ramanan16@users.noreply.github.com>
2025-07-24 12:47:03 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
-
+//for-loop
         for(int i = 1 ; i <= 5 ; i++){
             System.out.println(i + "Hi!");
         }
